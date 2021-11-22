a05daf0de2f5b09bab0b81117ba2a556354eb72a
magnus <magnus@gmail.com>
2021-11-22 10:09:10 +0100

    test

--- data/data.js
+++ data/data.js
@@ -1,1 +1,19 @@
-hej
+const express = require("express");
+const app = express();
+
+// Controllers
+//const userController = require("./src/controllers/user-controller");
+
+const PORT = process.env.PORT || 3000;
+
+
+// Middleware - endnu et fedt term
+app.use(express.static("./src/views"));
+// Kommer som string -> JSON
+app.use(express.json());
+
+// Routes
+//app.use("/users", userController);
+
+// Start server
+app.listen(PORT, console.log("Server is running"));
